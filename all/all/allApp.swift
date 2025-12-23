32082391105672220a3c9479d4313ec90903ebab
perrine022 <perrine.honore@gmail.com>
2025-12-23 17:40:28 +0100

    First commit

---
+++ all/all/allApp.swift
@@ -0,0 +1,17 @@
+//
+//  allApp.swift
+//  all
+//
+//  Created by Perrine Honoré on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct allApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ all/allTests/allTests.swift
@@ -0,0 +1,17 @@
+//
+//  allTests.swift
+//  allTests
+//
+//  Created by Perrine Honoré on 23/12/2025.
+//
+
+import Testing
+@testable import all
+
+struct allTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
